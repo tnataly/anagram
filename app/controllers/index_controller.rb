d8918eccf14ca28288533d1987bd047bcb52cbee
Nataly Tkachuk <techgirlua@gmail.com>
2015-05-22 10:49:48 +0300

    Adding error message

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,3 +1,29 @@
+def three_letter?(input)
+	if input.length == 3 
+		return true
+	else
+		return false
+	end
+end
+
+def distinct_letters?(input)
+	letter_array = input.chars
+	unique_letters = input.split("").uniq.join
+	if unique_letters.length < letter_array.length
+		false
+	else
+		true
+	end
+end
+
+def valid_input?(input)
+	if three_letter?(input) && distinct_letters?(input)
+		true
+	else
+		false
+	end
+end
+
 get '/anagrams/:word' do
 	@word = params[:word]
 	@anagrams = Word.find_anagrams(@word)
@@ -10,5 +36,10 @@ end
 
 post '/' do
 	@word = params[:word]
-	redirect "/anagrams/#{@word}"	
+	if valid_input?(@word)
+		redirect "/anagrams/#{@word}"	
+	else
+		@error = "Your word is not valid<br>"
+		erb :index
+	end
 end
