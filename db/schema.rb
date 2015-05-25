4f0f96f21b8e25b6bbd2c747d01efd161cdf7fca
Nataly Tkachuk <techgirlua@gmail.com>
2015-05-25 13:27:20 +0300

    Adding a letters column and populating it

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150522051947) do
+ActiveRecord::Schema.define(version: 20150525101905) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "words", force: :cascade do |t|
     t.string "text"
+    t.string "letters"
   end
 
 end
